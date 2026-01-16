ddda600110a6eb56c2f18161e40c13c64a8dd3cb
Samim-786 <aktarsamim786921@gmail.com>
2026-01-16 20:56:15 +0530

    Fix Inngest build error

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/lib/inngest";   // your client
-import { syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/inngest/functions"; 
+import { inngest } from "..//..//..//inngest/client";   // your client
+import { syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/inngest/function"; 
 // or wherever your functions are actually stored
 
 export const { GET, POST, PUT } = serve({
